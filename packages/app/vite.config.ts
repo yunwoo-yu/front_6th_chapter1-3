e779fed148d0093af67ac8c74cef757b1d99396f
junilhwang <junil.h@kakao.com>
2025-07-19 12:16:42 +0900

    fix: base url 수정

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react-oxc";
 import { createViteConfig } from "../../createViteConfig";
 
-const base: string = process.env.NODE_ENV === "production" ? "/front_6th_chapter1-2/" : "";
+const base: string = process.env.NODE_ENV === "production" ? "/front_6th_chapter1-3/" : "";
 
 export default createViteConfig({
   base,
