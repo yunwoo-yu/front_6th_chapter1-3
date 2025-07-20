8d123269c465aa81e16907527758faf0d667fe83
Ycow <skypnal12@gmail.com>
2025-07-20 18:04:36 +0900

    feat: shallowEquals add

--- packages/lib/src/equals/shallowEquals.ts
+++ packages/lib/src/equals/shallowEquals.ts
@@ -1,3 +1,23 @@
 export const shallowEquals = (a: unknown, b: unknown) => {
-  return a === b;
+  if (a === b) {
+    return true;
+  }
+
+  if (typeof a !== "object" || a === null || typeof b !== "object" || b === null) {
+    return false;
+  }
+
+  const keysA = Object.keys(a) as (keyof typeof a)[];
+  const keysB = Object.keys(b) as (keyof typeof b)[];
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (const key of keysA) {
+    if (!Object.hasOwn(b, key) || a[key] !== b[key]) {
+      return false;
+    }
+  }
+  return true;
 };
