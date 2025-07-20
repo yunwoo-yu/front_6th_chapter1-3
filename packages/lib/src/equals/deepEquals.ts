b42b1955375728dda26a99868174f527ffb3bfa1
Ycow <skypnal12@gmail.com>
2025-07-20 18:05:08 +0900

    feat: deepEquals add

--- packages/lib/src/equals/deepEquals.ts
+++ packages/lib/src/equals/deepEquals.ts
@@ -1,3 +1,38 @@
 export const deepEquals = (a: unknown, b: unknown) => {
-  return a === b;
+  if (a === b) {
+    return true;
+  }
+
+  if (typeof a !== "object" || a === null || typeof b !== "object" || b === null) {
+    return false;
+  }
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if (a.length !== b.length) {
+      return false;
+    }
+
+    for (let i = 0; i < a.length; i++) {
+      if (!deepEquals(a[i], b[i])) return false;
+    }
+
+    return true;
+  }
+
+  if (typeof a === "object" && typeof b === "object") {
+    const keysA = Object.keys(a);
+    const keysB = Object.keys(b);
+
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+
+    for (const key of keysA) {
+      if (!Object.hasOwn(b, key) || !deepEquals(a[key as keyof typeof a], b[key as keyof typeof b])) return false;
+    }
+
+    return true;
+  }
+
+  return true;
 };
