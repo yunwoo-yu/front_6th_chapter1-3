407cb7f4afb7975b580bec477178e238f3bb3280
Ycow <skypnal12@gmail.com>
2025-07-20 18:24:25 +0900

    feat: add useAutoCallback

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -1,7 +1,15 @@
 import type { AnyFunction } from "../types";
-// import { useCallback } from "./useCallback";
-// import { useRef } from "./useRef";
+import { useCallback } from "./useCallback";
+import { useRef } from "./useRef";
 
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  const fnRef = useRef<T>(fn);
+
+  // 렌더링 시점에 매번 업데이트
+  fnRef.current = fn;
+
+  const memoizedCallback = useCallback(() => fnRef.current(), []);
+
+  // 항상 같은 참조를 반환하되, 내부에서 최신 fn을 호출
+  return memoizedCallback as T;
 };
