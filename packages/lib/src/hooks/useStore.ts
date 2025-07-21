ed1aec89771cf1da7196afdadcc9da476946d22f
Ycow <skypnal12@gmail.com>
2025-07-21 11:11:36 +0900

    feat: useStore add

--- packages/lib/src/hooks/useStore.ts
+++ packages/lib/src/hooks/useStore.ts
@@ -1,3 +1,4 @@
+import { useSyncExternalStore } from "react";
 import type { createStore } from "../createStore";
 // import { useSyncExternalStore } from "react";
 import { useShallowSelector } from "./useShallowSelector";
@@ -9,5 +10,9 @@ const defaultSelector = <T, S = T>(state: T) => state as unknown as S;
 export const useStore = <T, S = T>(store: Store<T>, selector: (state: T) => S = defaultSelector<T, S>) => {
   // useSyncExternalStore와 useShallowSelector를 사용해서 store의 상태를 구독하고 가져오는 훅을 구현해보세요.
   const shallowSelector = useShallowSelector(selector);
-  return shallowSelector(store.getState());
+
+  return useSyncExternalStore(
+    (onStoreChange) => store.subscribe(onStoreChange),
+    () => shallowSelector(store.getState()),
+  );
 };
