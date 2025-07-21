d47bd0b12e6702d2294c28af55f9c3e12a9dc980
Ycow <skypnal12@gmail.com>
2025-07-21 12:39:36 +0900

    fix: useAutoCallback args 추가

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -8,7 +8,7 @@ export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
   // 렌더링 시점에 매번 업데이트
   fnRef.current = fn;
 
-  const memoizedCallback = useCallback(() => fnRef.current(), []);
+  const memoizedCallback = useCallback((...args: Parameters<T>) => fnRef.current(...args), []);
 
   // 항상 같은 참조를 반환하되, 내부에서 최신 fn을 호출
   return memoizedCallback as T;
