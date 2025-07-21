3636df7170a161891d78d6e4481a9b30d14181a1
Ycow <skypnal12@gmail.com>
2025-07-21 11:10:27 +0900

    feat: subscribe 함수가 구독해제를 반환하도록 변경

--- packages/lib/src/createObserver.ts
+++ packages/lib/src/createObserver.ts
@@ -6,11 +6,12 @@ export const createObserver = () => {
   // useSyncExternalStore 에서 활용할 수 있도록 subscribe 함수를 수정합니다.
   const subscribe = (fn: Listener) => {
     listeners.add(fn);
-  };
 
-  // const unsubscribe = (fn: Listener) => {
-  //   listeners.delete(fn);
-  // };
+    // unsubscribe 함수를 반환
+    return () => {
+      listeners.delete(fn);
+    };
+  };
 
   const notify = () => listeners.forEach((listener) => listener());
 
