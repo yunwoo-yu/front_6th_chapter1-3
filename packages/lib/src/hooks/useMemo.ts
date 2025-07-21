29643bff686e08592f92ec07a6a571cdc6bc43f6
Ycow <skypnal12@gmail.com>
2025-07-22 01:12:59 +0900

    refactor: useMemo 조건식 개선, 타입단언 제거

--- packages/lib/src/hooks/useMemo.ts
+++ packages/lib/src/hooks/useMemo.ts
@@ -5,15 +5,12 @@ import { useRef } from "./useRef";
 export function useMemo<T>(factory: () => T, _deps: DependencyList, _equals = shallowEquals): T {
   const valueRef = useRef<T | null>(null);
   const depsRef = useRef<DependencyList>([]);
-  const isFirstRenderRef = useRef(true);
 
-  // 첫 번째 렌더링이거나 의존성이 변경되었는지 확인
-  if (isFirstRenderRef.current || !_equals(depsRef.current, _deps)) {
-    // 변경되었다면 factory 실행하고 결과 저장
+  if (valueRef.current === null || !_equals(depsRef.current, _deps)) {
+    // 변경되었거나 첫 렌더링인 경우 factory 실행하고 결과 저장
     valueRef.current = factory();
     depsRef.current = _deps;
-    isFirstRenderRef.current = false;
   }
 
-  return valueRef.current as T;
+  return valueRef.current;
 }
